6ba0f42df50857bb2509771929775f4b2ed3b532
parkminseon <pms136346@gmail.com>
2020-07-01 21:27:18 +0900

    c총알 피하가 2일차

---
+++ Dodge/Assets/Dodge/Scripts/Bullet.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public float m_speed = 5f;
+    public float m_DestroyCooltime = 5f;
+    // Update is called once per frame
+    void Update()
+    {
+        Rigidbody rigidibody = /*gameObject*/GetComponent<Rigidbody>();
+
+
+        rigidibody.AddForce(transform.forward * m_speed);
+
+        m_DestroyCooltime = m_DestroyCooltime- Time.deltaTime;
+
+        if (m_DestroyCooltime <= 0)
+            gameObject.SetActive(false);
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.attachedRigidbody.tag == "Player")
+        {
+            var player = other.attachedRigidbody.GetComponent<PlayerController>();
+            player.Die();
+        }
+        
+            
+    }
+}
