c745d714450e564bbd73ae0e95ceb3403c0c1ba3
parkminseon <pms136346@gmail.com>
2020-07-15 21:11:30 +0900

    유니런3일차

---
+++ UniRun/Assets/UniRun/Scriptes/Background.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    private float width;
+
+    private void Awake()
+    {
+        var collider = GetComponent<BoxCollider2D>();
+        float width = collider.size.x;
+    }
+
+    void Update()
+    {
+        
+
+        if (transform.position.x <= -width)
+        {
+            transform.position 
+                += Vector3.right * 2f * width;
+        }
+    }
+}
