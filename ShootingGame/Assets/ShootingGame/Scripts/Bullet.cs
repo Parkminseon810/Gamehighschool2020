93e307afde1bde0b300be7d2b02d106d42cf3122
parkminseon <pms136346@gmail.com>
2020-07-20 21:07:34 +0900

    슈팅게임 1일차

---
+++ ShootingGame/Assets/ShootingGame/Scripts/Bullet.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float m_Speed = 2f;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Vector3 movement = transform.up * m_Speed * Time.deltaTime;
+        transform.position += movement;
+    } 
+}
---
+++ ShootingGame/Assets/ShootingGame/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    
+    public float m_Speed = 10f;
+    // Update is called once per frame
+    void Update()
+    {
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+
+        Vector3 velocity = (new Vector3(xAxis, yAxis, 0).normalized * m_Speed);
+
+        Vector3 movement = velocity * Time.deltaTime;
+        transform.position = transform.position + movement;
+
+    }
+}
